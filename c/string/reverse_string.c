e681e8d046a2e0005b5720779b61b455ffd676fd
clamyang <ybq2888@163.com>
2022-10-09 15:52:48 +0800

    change the way to get string length

--- c/string/reverse_string.c
+++ c/string/reverse_string.c
@@ -1,11 +1,12 @@
 // #344 反转字符串
 #include <stdio.h>
+#include <string.h>
 
 void main(void)
 {
     char hello[] = {'h', 'e', 'l', 'l', 'o'};
 
-    int length = 5;
+    int length = strlen(hello);
     for (int i = 0, j = length - 1; i < j; i++, j--)
     {
         char single = hello[i];
