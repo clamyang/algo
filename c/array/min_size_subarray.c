75553b773f6da13ba50b87928bd75983dbf9bdde
clamyang <ybq2888@163.com>
2022-10-09 14:14:32 +0800

    add sliding window soluation for #209

--- c/array/min_size_subarray.c
+++ c/array/min_size_subarray.c
@@ -1,16 +1,20 @@
+// # 209 最小长度子数组
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int min_size_subarr(int nums[], int length, int target);
+int min_size_sliding_window(int nums[], int length, int target);
 
 int main(int argc, int *argv[])
 {
-  int nums[] = {1, 2, 5, 2};
-  int target = 8;
+  int nums[] = {1, 2, 5, 2, 6};
+  int target = 10;
 
   int length = sizeof(nums) / sizeof(nums[0]);
 
-  printf("%d\n", min_size_subarr(nums, length, target));
+  // printf("%d\n", min_size_subarr(nums, length, target));
+  printf("%d\n", min_size_sliding_window(nums, length, target));
   return 0;
 }
 
@@ -37,5 +41,28 @@ int min_size_subarr(int nums[], int length, int target)
       }
     }
   }
+  return min_val;
+}
+
+int min_size_sliding_window(int nums[], int length, int target)
+{
+  int sum = 0;
+  int i = 0, j = 0;
+  int min_val = length;
+
+  for (j = 0; j < length; j++)
+  {
+    sum += nums[j];
+    while (sum >= target)
+    {
+      if (min_val > (j - i + 1))
+      {
+        min_val = (j - i + 1);
+      }
+
+      sum -= nums[i++];
+    }
+  }
+
   return min_val;
 }
