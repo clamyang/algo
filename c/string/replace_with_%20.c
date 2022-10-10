1c7e1d74acd001b210ed0d49e7ba86df066ef67f
clamyang <ybq2888@163.com>
2022-10-10 15:16:07 +0800

    add double pointer implement

--- c/string/replace_with_%20.c
+++ c/string/replace_with_%20.c
@@ -4,12 +4,14 @@
 #include <stdlib.h>
 
 char *replace_string(char statement[]);
-char *replace_string_hard(char statement[]);
+void replace_string_hard(char statement[]);
 
 void main(void)
 {
     char happy[] = "We are happy";
-    replace_string(happy);
+    // replace_string(happy);
+
+    replace_string_hard(happy);
 }
 
 char *replace_string(char statement[])
@@ -24,8 +26,8 @@ char *replace_string(char statement[])
         }
     }
 
-    int new_length = length + 3 * space_count - space_count;
-    char *new_str = (char *)malloc(sizeof(char) * new_length);
+    int new_length = length + 2 * space_count;
+    char *new_str = (char *)malloc(sizeof(char) * new_length + 1);
 
     for (size_t i = 0, j = 0; i < length && j < new_length; i++)
     {
@@ -41,14 +43,34 @@ char *replace_string(char statement[])
         }
     }
 
-    for (size_t i = 0; i < new_length; i++)
-    {
-        printf("%c ", new_str[i]);
-    }
+    new_str[new_length] = '\0';
 
     return new_str;
 }
 
-char *replace_string_hard(char statement[])
+void replace_string_hard(char statement[])
 {
+    // 既然已经使用 malloc 分配了一个 O(n) 的字符串数组
+    // 那直接同上面的方法相同就行了..
+    // 以下内容是为了练习双指针的解题方法
+
+    // 假设扩容好的字符数组如下
+    char happy[] = "We are happy    ";
+    int new_len = strlen(happy);
+    int old_len = strlen(statement);
+
+    for (int i = old_len, j = new_len; i > 0; i--, j--)
+    {
+        if (statement[i] != ' ')
+        {
+            happy[j] = statement[i];
+            j;
+        }
+        else
+        {
+            happy[j--] = '0';
+            happy[j--] = '2';
+            happy[j] = '%';
+        }
+    }
 }
