6b4a719168afda217d970b3ddb141a89c31c2910
clamyang <ybq2888@163.com>
2022-10-26 13:44:35 +0800

    添加迭代法和层序遍历的方式

--- go/binarytree/maxDepth.go
+++ go/binarytree/maxDepth.go
@@ -1,5 +1,7 @@
 package binarytree
 
+import "container/list"
+
 /*
  * 二叉树深度：指从根节点到该节点的最长简单路径边的条数或者节点数（取决于深度从0开始还是从1开始）
  *
@@ -26,7 +28,54 @@ func max(x, y int) int {
 	return y
 }
 
+var Ans int
+
 // MaxDepthPre 前序遍历
-func MaxDepthPre(root *TreeNode) int {
-	return 0
+func MaxDepthPre(root *TreeNode, depth int) {
+	if depth > Ans {
+		Ans = depth
+	}
+
+	if root.left == nil && root.right == nil {
+		return
+	}
+
+	if root.left != nil {
+		depth++
+		MaxDepthPre(root.left, depth)
+		depth--
+	}
+
+	if root.right != nil {
+		depth++
+		MaxDepthPre(root.right, depth)
+		depth--
+	}
+}
+
+// MaxDepthIterate 迭代法
+func MaxDepthIterate(root *TreeNode) {
+	if root == nil {
+		return
+	}
+
+	queue := list.New()
+	queue.PushBack(root)
+
+	var layer int
+	for queue.Len() != 0 {
+		size := queue.Len()
+		layer++
+		for i := 0; i < size; i++ {
+			head := queue.Remove(queue.Front()).(*TreeNode)
+
+			if head.left != nil {
+				queue.PushBack(head.left)
+			}
+
+			if head.right != nil {
+				queue.PushBack(head.right)
+			}
+		}
+	}
 }
